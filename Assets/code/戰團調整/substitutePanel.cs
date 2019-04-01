8293372bee02b307431e327b46927b16a574f1cb
sasalicat <qq695729652@gmail.com>
2019-04-01 21:26:38 +0800

    構思了備選單位面板,但還是有scale上的問題

---
+++ Assets/code/戰團調整/substitutePanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class substitutePanel : MonoBehaviour {
+    public GameObject panel;
+    public GameObject headPrafeb;
+    List<GameObject> heads = new List<GameObject>();
+    public const float x_start = 0.013f;
+    public const float x_offset = 0.09f-x_start; 
+	// Use this for initialization
+	void Start () {
+        createHead(0, null, null);
+        createHead(1, null, null);
+        createHead(2, null, null);
+	}
+
+    public void createHead(int race,List<int> skillNos,List<int> itemNos)
+    {
+        GameObject headIcon= Instantiate(headPrafeb, panel.transform);
+        headIcon.transform.localPosition = new Vector2(x_start+x_offset+ 2 * x_offset * heads.Count,0);
+        heads.Add(headIcon);
+    }
+}
