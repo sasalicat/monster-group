c3689c0f3ea345f29b5a5d441a279e57f800751b
sasalicat <waimaoisme@126.com>
2019-04-03 23:12:04 +0800

    嘗試自動佈置格子不過不太成功

--- Assets/code/戰團調整/teamPanel.cs
+++ Assets/code/戰團調整/teamPanel.cs
@@ -10,13 +10,46 @@ public class teamPanel : MonoBehaviour {
     public void init(List<RoleRecord> units, vec2i size)
     {
         this.units = units;
-        Vector2 startPoint = Camera.main.ScreenToWorldPoint(new Vector2(0,Camera.main.pixelHeight*0.5f));
-        widthPercentage = (1-gapPercentage)/
+        Vector2 startPoint;
+        float gapSize = 0;
+        float girdSize = 0;
+        float fieldAspect = Camera.main.pixelWidth / (0.5f * Camera.main.pixelWidth);
+        float girdAspect = size.x / size.y;
+        float unitPixel = girds[0].GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
+        float spriteSize = girds[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        float cameraHeight_half = Camera.main.orthographicSize;
+        float cameraWidth_half = Camera.main.orthographicSize * Camera.main.aspect;
+        Debug.Log("cameraWidth_half:" + cameraWidth_half);
+        Debug.Log("cameraHeight_half:" + cameraHeight_half);
+        if (fieldAspect > girdAspect)
+        {
+
+            int boundWidth = (int)(Camera.main.pixelWidth - 0.5f * Camera.main.pixelHeight);
+
+            startPoint = Camera.main.ScreenToWorldPoint(new Vector2(boundWidth,Camera.main.pixelHeight*0.5f));
+            Debug.Log("boundWidth:" + boundWidth + " screen space start position:" + new Vector2(boundWidth, Camera.main.pixelHeight * 0.5f) + " world space start position:" + startPoint);
+            gapSize = (cameraHeight_half * gapPercentage / (size.y + 1));
+            girdSize = (cameraHeight_half *  (1-gapPercentage) / size.y);
+            Debug.Log("girdSize:" + girdSize);
+        }
+        else
+        {
+            startPoint = Camera.main.ScreenToWorldPoint(new Vector2(0, Camera.main.pixelHeight * 0.5f));
+            gapSize = cameraWidth_half * gapPercentage / (size.y + 1)/unitPixel;
+            girdSize = cameraWidth_half * (1 - gapPercentage) / size.y/unitPixel;
+        }
+        //widthPercentage = (1-gapPercentage)/
+        Vector2 gapVec = new Vector2(gapSize, gapSize);
+        Vector2 halfGridVec = new Vector2(girdSize / 2, girdSize / 2);
+        float gScale = girdSize / spriteSize;
+        Debug.Log("girdSize:" + girdSize + " spriteSize:" + spriteSize + "gscale:" + gScale);
         for (int y = 0; y < size.y; y++)
         {
             for(int x = 0; x < size.x; x++)
             {
-                Vector2 
+                Vector2 pos = startPoint + gapVec + new Vector2(x * (gapSize + girdSize), y * (gapSize + girdSize)) -halfGridVec;
+                
+                createGrid(pos, gScale, y * size.x + x);
             }
         }
     }
@@ -30,7 +63,9 @@ public class teamPanel : MonoBehaviour {
         Sprite sprite = girds[0].GetComponent<SpriteRenderer>().sprite;
         float aspect = sprite.bounds.size.x / sprite.bounds.size.y;
         scale = panelFix.getScale(aspect, sprite.bounds.size);
-	}
+        init(null, new vec2i(5, 4));
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
