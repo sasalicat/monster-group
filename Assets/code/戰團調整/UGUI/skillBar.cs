226e249f2135ae7c7966f6af1e655243be9db7fb
sasalicat <waimaoisme@126.com>
2019-05-28 22:30:26 +0800

    實做了skillbar ,還沒有debug 用了Layout 來方便排版

--- Assets/code/戰團調整/UGUI/skillBar.cs
+++ Assets/code/戰團調整/UGUI/skillBar.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class skillBar : MonoBehaviour
 {
     public RoleRecord role;
+    public GameObject IconPrab; 
     public void init(RoleRecord data)
     {
         role = data;
         foreach (int no in role.skillNos)
         {
-            記得做createheadIcon
+            //記得做createheadIcon
+            Instantiate(IconPrab,transform);
+            IconPrab.GetComponent<Image>().sprite = ImageList.main.headIcons[no];
         }
     }
 }
