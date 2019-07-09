f937d845860a890f77b75f1b223b17d6d972ee05
sasalicat <waimaoisme@126.com>
2019-07-09 10:49:11 +0800

    設計了表示職業信息的類別

---
+++ Assets/code/基礎類別/careerInf.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface careerInf{
+    int careerNo
+    {
+        get;
+    }
+    string name
+    {
+        get;
+    }
+    string Commentary
+    {
+        get;
+    }
+    List<int> skillPool
+    {
+        get;
+    }
+    int nexrCareer
+    {
+        get;
+    }
+}
