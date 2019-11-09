0d951584799ec7c42b7f4b726d8a2644fcf3b74c
sasalicat <waimaoisme@126.com>
2019-11-09 22:29:30 +0800

    新遊戲的準備 新遊戲會使用大部分舊的戰鬥機制 同時新增了大量能combo的角色行動機制,目前新增了unitData_v2來新增這些對應的數值

---
+++ Assets/newGame/unitData_v2.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unitData_v2 : unitData {
+    const int STAND_DODGE = 0;
+    const int STAND_BLOCK = 0;
+    const int STAND_INSIGHT = 0;
+    const int STAND_CRIT = 0;
+    const int STAND_BATTER = 0;
+    const int STAND_COUNTER = 0;
+
+    private int dodge_point = STAND_DODGE;
+    public int Now_Dodge_Point
+    {
+        get
+        {
+            return dodge_point;
+        }
+        set
+        {
+            dodge_point = value;
+        }
+    }
+
+    private int block_point = STAND_BLOCK;
+    public int Now_Block_Point
+    {
+        get
+        {
+            return block_point;
+        }
+        set
+        {
+            block_point = value;
+        }
+    }
+
+    private int insight_point = STAND_INSIGHT;
+    public int Now_Insight_Point
+    {
+        get
+        {
+            return insight_point;
+        }
+        set
+        {
+            insight_point = value;
+        }
+    }
+
+    private int crit_point = STAND_CRIT;
+    public int Now_Crit_Point
+    {
+        get
+        {
+            return crit_point;
+        }
+        set
+        {
+            crit_point = value;
+        }
+    }
+
+    private int batter_point = STAND_BATTER;
+    public int Now_Batter_Point
+    {
+        get
+        {
+            return batter_point;
+        }
+        set
+        {
+            batter_point = value;
+        }
+    }
+
+    private int counter_point = STAND_COUNTER;
+    public int Now_Counter_Point
+    {
+        get
+        {
+            return counter_point;
+        }
+        set
+        {
+            counter_point = value;
+        }
+    } 
+
+}
