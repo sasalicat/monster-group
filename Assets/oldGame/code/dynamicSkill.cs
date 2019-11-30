6953907bceab24a464ae473e9ea31a5eeadb7329
sasalicat <waimaoisme@126.com>
2019-11-30 21:30:57 +0800

    正在構思新的技能架構

---
+++ Assets/oldGame/code/dynamicSkill.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class dynamicSkill : CDSkill {
+    protected List<modifier> modifierList;
+    protected abstract List<modifier> Modifiers
+    {
+        get;
+    } 
+    public override void onInit(unitControler owner, Callback4Unit deleg)
+    {
+        modifierList = Modifiers;
+        foreach(modifier mod in modifierList)
+        {
+            mod.traget = this;
+            mod.onSkillInit(owner, deleg);
+        }
+    }
+    public abstract unitControler[] getTragets(Environment env);
+    public override unitControler[] findTraget(Environment env)
+    {
+        unitControler[] tragets= getTragets(env);
+        foreach (modifier mod in modifierList)
+        {
+            mod.onFindTraget(env,ref tragets);
+        }
+        return tragets;
+    }
+
+}
---
+++ Assets/oldGame/code/modifier.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class modifier{
+    public CDSkill traget;
+    public abstract void onSkillInit(unitControler owner, Callback4Unit deleg);
+    //public abstract void ModifyDamager(Damage d);
+    //public abstract void aftSkillEnd();
+    public abstract void onFindTraget(Environment env,ref unitControler[] tragets);
+}
