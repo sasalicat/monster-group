d00fb912542934ae360b3a0fa0408489d4cb77f4
sasalicat <waimaoisme@126.com>
2019-12-16 10:22:56 +0800

    導入了武士素材,下一步想下怎麼修改武士的近戰普通攻擊被擊特效

--- Assets/newGame/code/IconInBattle.cs
+++ Assets/newGame/code/IconInBattle.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class IconInBattle : MonoBehaviour{
-    public const float DEFAULT_SHOW_TIME = 0.5f;
+    public const float DEFAULT_OPAQUE_TIME = 0.4f;
+    public const float DEFAULT_GRADIENT_TIME = 0.4f;
+    protected float opaqueTimeLeft = 0;
     protected float timeLeft = 0;
     protected float lastTimeMax = 1;
     protected SpriteRenderer render;
@@ -13,14 +15,16 @@ public class IconInBattle : MonoBehaviour{
     //public Sprite iconl;
     public void show(Sprite icon)
     {
-        show(icon, DEFAULT_SHOW_TIME);
+        show(icon, DEFAULT_OPAQUE_TIME, DEFAULT_OPAQUE_TIME);
     }
-    public void show(Sprite icon,float second)
+    public void show(Sprite icon,float opaque_time,float left_time)
     {
         gameObject.SetActive(true);
         render.sprite = icon;
-        timeLeft = second;
-        lastTimeMax = second;
+        opaqueTimeLeft = opaque_time;
+        timeLeft = left_time;
+        lastTimeMax = left_time;
+        render.color = Color.white;
     }
     public void Start()
     {
@@ -28,16 +32,23 @@ public class IconInBattle : MonoBehaviour{
     }
     public void Update()
     {
-        timeLeft -= Time.deltaTime;
-        Color c = render.color;
-        c.a = timeLeft / lastTimeMax;
-        render.color = c;
-        if (timeLeft <= 0)
+        if (opaqueTimeLeft > 0)//第一部分不透明
         {
-            if (forNextEnd != null)
-                forNextEnd();
-            forNextEnd = null;
-            gameObject.SetActive(false);
+            opaqueTimeLeft -= Time.deltaTime;
+        }
+        else//第二部分漸隱
+        {
+            timeLeft -= Time.deltaTime;
+            Color c = render.color;
+            c.a = timeLeft / lastTimeMax;
+            render.color = c;
+            if (timeLeft <= 0)
+            {
+                if (forNextEnd != null)
+                    forNextEnd();
+                forNextEnd = null;
+                gameObject.SetActive(false);
+            }
         }
     }
 }
