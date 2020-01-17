ff5492c72e376844b81bdeab79d09b2afda1f274
sasalicat <qq695729652@gmail.com>
2020-01-17 20:41:53 +0800

    正在思考怎麼做forSkillTrageting的modfier,因為trigger傳的是Dictionary,如果把ref unitControler[] tragets直接放進Dictionary里,在trigger里new從Dictionary里拿出來的陣列,並不會影響到真正的tragets

---
+++ Assets/newGame/code/Skill/双重攻击/newSkill_doubleHit.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newSkill_doubleHit : dynamicSkill {
+    public override float StandCoolDown
+    {
+        get
+        {
+            return -1;
+        }
+    }
+
+    public override bool canUse
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+    public override unitControler[] getTragets(Environment env)
+    {
+        return null;
+    }
+
+    public override SkillInf Inf()
+    {
+        return new SkillInf_v2(this);
+    }
+
+    public override void trigger(Dictionary<string, object> args)
+    {
+        unitControler[] tragets = (unitControler[])args["tragets"];
+        unitControler[] enemys = ((comboManager)comboManager.main).ChessBoard.enemyOf(Owner);
+        if (enemys.Length <= tragets.Length)
+        {
+
+        }
+        else
+        {
+            List<unitControler> candidate = new List<unitControler>();
+            foreach (unitControler unit in enemys)
+            {
+                bool found = false;
+                foreach (unitControler traget in tragets)
+                {
+                    if (traget == unit)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)//如果敌人不在目标中
+                {
+                    candidate.Add(unit);
+                }
+            }
+            if (candidate.Count <= 0)
+            {
+                return;
+            }
+            int num = Randomer.main.getInt();
+            int index = num % candidate.Count;
+            comboControler newTraget = (comboControler)candidate[index];
+            newTraget._beAppoint(information, args);
+            List<unitControler> newTragets = new List<unitControler>(tragets);
+            newTragets.Add(newTraget);
+            tragets = newTragets.ToArray();
+        }
+    }
+}
---
+++ Assets/newGame/code/Skill/技能修改器脚本/mod_1_baseAttackTragetting.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mod_1_baseAttackTragetting : modifier
+{
+    public void befSkill(SkillInf skillInf, Dictionary<string, object> skillArgs, ref unitControler[] tragets)
+    {
+        unitControler[] enemys = ((comboManager)comboManager.main).ChessBoard.enemyOf(traget.Owner);
+        if (enemys.Length <= tragets.Length)
+        {
+
+        }
+        else
+        {
+            List<unitControler> candidate = new List<unitControler>();
+            foreach(unitControler unit in enemys)
+            {
+                bool found = false;
+                foreach(unitControler traget in tragets)
+                {
+                    if(traget == unit)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)//如果敌人不在目标中
+                {
+                    candidate.Add(unit);
+                }
+            }
+            if (candidate.Count <= 0) {
+                return;
+            }
+            int num= Randomer.main.getInt();
+            int index = num % candidate.Count;
+            comboControler newTraget = (comboControler)candidate[index];
+            newTraget._beAppoint(traget.information, skillArgs);
+            List<unitControler> newTragets = new List<unitControler>(tragets);
+            newTragets.Add(newTraget);
+            tragets = newTragets.ToArray();
+        }
+    }
+    public override void onSkillInit(unitControler owner, Callback4Unit_v2 deleg)
+    {
+        deleg._BefUseSkill += befSkill;
+    }
+}
